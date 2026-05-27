2b1824223fce2f2f616e073ed98134a9bd6a27ac
stellacolari <stellacolari@gmail.com>
2026-05-26 21:47:01 -0300

    ajusta posicao da secao de variacoes no produto

--- components/produtos/NovoProdutoClient.tsx
+++ components/produtos/NovoProdutoClient.tsx
@@ -610,14 +610,13 @@ export default function NovoProdutoClient({
               </Field>
             </div>
           </AccordionSection>
-
+          <ProdutoGaleriaInput name="galeriaProduto" />
           <AccordionSection
             title="Imagens do produto"
             description="A imagem 1 será a principal e a imagem 2 será usada no hover."
             defaultOpen
           >
             <VariacoesProdutoInput name="variacoesProduto" />
-            <ProdutoGaleriaInput name="galeriaProduto" />
           </AccordionSection>
 
           <AccordionSection
